c87f5ba6d7c9ab2c6b9d88842d126eddb0025d5f
yuan <mlovyuan@yahoo.com.tw>
2023-05-21 22:42:46 +0800

    add EF_tools package and annotation into models

--- UdemyTravel/Models/TouristRoutePicture.cs
+++ UdemyTravel/Models/TouristRoutePicture.cs
@@ -1,10 +1,17 @@
-﻿namespace UdemyTravel.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UdemyTravel.Models
 {
     public class TouristRoutePicture
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Url { get; set; }
+        [ForeignKey("TouristRouteId")]
         public Guid TouristRouteId { get; set; }
+        // create releation to foreign key 
         public TouristRoute TouristRoute { get; set; }
     }
 }
