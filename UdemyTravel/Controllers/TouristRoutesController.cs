b065b96fa0f5d2e49c04de21a14ae01903b21c4f
yuan <mlovyuan@yahoo.com.tw>
2023-06-11 23:07:07 +0800

    add http status logic

--- UdemyTravel/Controllers/TouristRoutesController.cs
+++ UdemyTravel/Controllers/TouristRoutesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using UdemyTravel.Services;
 
 namespace UdemyTravel.Controllers
@@ -17,7 +18,15 @@ namespace UdemyTravel.Controllers
         [HttpGet]
         public IActionResult GetAllTouristRotes()
         {
-            return Ok(_touristRouteRepository.GetAllTouristRoute());
+            var touristRoutes = _touristRouteRepository.GetAllTouristRoute();
+            return touristRoutes.IsNullOrEmpty() ? NotFound("沒有旅遊路線") : Ok(touristRoutes);
+        }
+
+        [HttpGet("{touristRouteId:Guid}")]
+        public IActionResult GetTouristRotesById(Guid touristRouteId)
+        {
+            var touristRoute = _touristRouteRepository.GetTouristRoute(touristRouteId);
+            return touristRoute is null ? NotFound($"旅遊路線{touristRouteId}不存在") : Ok(touristRoute);
         }
     }
 }
